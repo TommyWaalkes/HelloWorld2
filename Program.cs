643eae8172e4052725a9eb61ca4028777ef51cbb
Tommy Waalkes <Tommy@grandcircus.co>
2019-01-17 09:22:32 -0500

    Added in second profound line

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("We're changing the world, one statement at time");
         }
     }
 }
